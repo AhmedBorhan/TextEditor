2223e7564b1f76417f803de94c863539860ab5b4
ahmed <ahmed.borhan.ab@gmail.com>
2020-10-26 14:23:04 +0300

    Updates

---
+++ precache-manifest.29fcaa9f89536c199cc2687d4f36fdfd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4da27df9d074299ce9723270d80de2",
+    "url": "/TextEditor/index.html"
+  },
+  {
+    "revision": "ed616ffb001040217bc8",
+    "url": "/TextEditor/static/css/2.e3cf4cf6.chunk.css"
+  },
+  {
+    "revision": "bd507b4de1416603de70",
+    "url": "/TextEditor/static/css/main.5daefb2d.chunk.css"
+  },
+  {
+    "revision": "ed616ffb001040217bc8",
+    "url": "/TextEditor/static/js/2.ad949155.chunk.js"
+  },
+  {
+    "revision": "0de5e2d4d1ff2c24be3b104dfb788a25",
+    "url": "/TextEditor/static/js/2.ad949155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd507b4de1416603de70",
+    "url": "/TextEditor/static/js/main.25f3bb99.chunk.js"
+  },
+  {
+    "revision": "9d34ff4bdc4fe3f33795",
+    "url": "/TextEditor/static/js/runtime-main.0220d122.js"
+  },
+  {
+    "revision": "28438a18eafdbeae3bf54a0e7930f273",
+    "url": "/TextEditor/static/media/delete.28438a18.svg"
+  },
+  {
+    "revision": "fa7651f7460dc57eeb334458f5db9d01",
+    "url": "/TextEditor/static/media/find.fa7651f7.svg"
+  },
+  {
+    "revision": "9e5413c0b1766c3f9b53a7dcc6588c75",
+    "url": "/TextEditor/static/media/floppy-disk.9e5413c0.svg"
+  },
+  {
+    "revision": "44133772b61dbb48120367fab3a53f77",
+    "url": "/TextEditor/static/media/plus.44133772.svg"
+  }
+]);
